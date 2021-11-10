2bc84e7e0a3ac8d15b108112012e978d60386736
Pierre Bajyana <pierre.bajyana@gmail.com>
2021-11-10 17:26:35 +0100

    theming added + a text component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MyText from './Text';
+console.log('MY TEXT: ', MyText);
 
 const styles = StyleSheet.create({
     container: {
@@ -14,7 +16,12 @@ const styles = StyleSheet.create({
 const Main = () => {
     return (
         <View style={styles.container}>
-            <Text>Rate Repository Application</Text>
+            <MyText>Simple text</MyText>
+            <MyText style={{ paddingBottom: 10 }}>Text with custom style</MyText>
+      <MyText fontWeight="bold" fontSize="subheading">
+        Bold subheading
+      </MyText>
+      <MyText color="textSecondary">Text with secondary color</MyText>
         </View>
     );
 };
