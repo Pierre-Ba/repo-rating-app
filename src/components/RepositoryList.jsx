acd3cfd896cf61636352a1d47c3d7f40bed38088
Pierre Bajyana <pierre.bajyana@gmail.com>
2021-11-19 17:03:24 +0100

    Created graphql directory with mutations, queries and fragments.js file. Changed the fetching in the RepositoryList component as we're now fetching repositories with Apollo Client using a graphql query. Exercise 10.11

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect,  } from 'react';
+import { useQuery } from '@apollo/client';
+import { FlatList, View, StyleSheet, Text } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
+import { GET_REPOSITORIES } from '../graphql/queries'
 console.log('USE REPOSITORIES', useRepositories);
 
 const styles = StyleSheet.create({
@@ -15,10 +17,24 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories()
+  
+  
+  
+ const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+   fetchPolicy: 'cache-and-network',
+   onError: (error) => {
+     console.log('ERROR: ', error.message)
+   }
+ })
 
-  const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+ if (loading) {
+   return <Text>loading...</Text>
+ }
+
+ console.log('DATA', data)
+
+  const repositoryNodes = data
+    ? data.repositories.edges.map(edge => edge.node)
     : [];
 
   return (
