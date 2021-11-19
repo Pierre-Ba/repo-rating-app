0034c36d35f0f858e0bcc414cda797e390953c3f
Pierre Bajyana <pierre.bajyana@gmail.com>
2021-11-19 15:32:54 +0100

    Added the ApolloProvider context to the app component.

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import  { SafeAreaView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import Main from './src/components/Main';
-import RepositoryList from './src/components/RepositoryList';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { NativeRouter } from 'react-router-native';
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ApolloProvider } from "@apollo/client";
+import Main from "./src/components/Main";
+import createApolloClient from "./src/utils/apolloClient";
+import { Provider as PaperProvider } from "react-native-paper";
+import { NativeRouter } from "react-router-native";
 
-
+const apolloClient = createApolloClient();
 
 const App = () => {
-  
-
   return (
-      <PaperProvider>  
-        <NativeRouter> 
-    <SafeAreaView>
-    <Main />
-    </SafeAreaView>
-    </NativeRouter>
-    <StatusBar style="auto"/>
+    <PaperProvider>
+      <NativeRouter>
+        <ApolloProvider client={apolloClient}>
+          <SafeAreaView>
+            <Main />
+          </SafeAreaView>
+        </ApolloProvider>
+      </NativeRouter>
+      <StatusBar style="auto" />
     </PaperProvider>
   );
-  
 };
 
 export default App;
